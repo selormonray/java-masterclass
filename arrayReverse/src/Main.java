6bada00869d09e9d5b35930090ce636e90658fc4
Selorm Ahorlu <selormonray14@gmail.com>
2024-10-14 23:09:24 +0000

    reverse element array

--- arrayReverse/src/Main.java
+++ arrayReverse/src/Main.java
@@ -35,5 +35,41 @@ public class Main {
         return values;
     }
 
+    private static int findMin(int[] array) {
+
+        int min = Integer.MAX_VALUE;
+        for (int el : array) {
+            if (el < min) {
+                min = el;
+            }
+        }
+
+        return min;
+    }
+
+    private static void reverse(int[] array) {
+
+        int maxIndex = array.length - 1;
+        int halfLength = array.length / 2;
+
+        for (int i = 0; i < halfLength; i++) {
+            int temp = array[i];
+            array[i] = array[maxIndex - i];
+            array[maxIndex - i] = temp;
+            System.out.println("--> " + Arrays.toString(array));
+        }
+    }
+
+    private static int[] reverseCopy(int[] array) {
+
+        int[] reversedArray = new int[array.length];
+        int maxIndex = array.length - 1;
+        for (int el : array) {
+            reversedArray[maxIndex--] = el;
+        }
+
+        return reversedArray;
+    }
+
 
 }
