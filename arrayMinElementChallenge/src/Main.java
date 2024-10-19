63e672243cd480153e7e76c772d29364c77a9879
Selorm Ahorlu <selormonray14@gmail.com>
2024-10-19 10:46:37 +0000

    reverse element array 2

--- arrayMinElementChallenge/src/Main.java
+++ arrayMinElementChallenge/src/Main.java
@@ -8,6 +8,8 @@ public class Main {
 
         int returnedMin = findMin(returnedArray);
         System.out.println("min = " + returnedMin);
+
+        reverse(returnedArray);
     }
 
 
@@ -37,5 +39,17 @@ public class Main {
         return min;
     }
 
+    private static void reverse(int[] array) {
+        int maxIndex = array.length - 1;
+        int halfLength = array.length / 2;
+
+        for (int i = 0; i < halfLength; i++) {
+            int temp = array[i];
+            array[i] = array[maxIndex - i];
+            array[maxIndex - i] = temp;
+
+            System.out.println("-->" + Arrays.toString(array));
+        }
+    }
 
 }
