0a543b8a866c4b4c5b120b324d5d9323ed08f8a1
Selorm Ahorlu <selormonray14@gmail.com>
2024-10-20 21:00:49 +0000

    multi-dimensional array

--- twoDimensionalArrays/src/Main.java
+++ twoDimensionalArrays/src/Main.java
@@ -35,5 +35,25 @@ public class Main {
 
         Object[] anyArray = new Object[3];
         System.out.println(Arrays.toString(anyArray));
+
+        anyArray[0] = new String[]{"a", "b", "c"};
+        System.out.println(Arrays.deepToString(anyArray));
+
+        anyArray[1] = new String[][]{
+                {"1", "2"},
+                {"3", "4", "5"},
+                {"6", "7", "8", "9"}
+        };
+        System.out.println(Arrays.deepToString(anyArray));
+
+        anyArray[2] = new int[2][2][2];
+        System.out.println(Arrays.deepToString(anyArray));
+
+
+        for (Object element : anyArray) {
+            System.out.println("Element type = " + element.getClass().getSimpleName());
+            System.out.println("Element toString() = " + element);
+            System.out.println(Arrays.deepToString((Object[]) element));
+        }
     }
 }
