93f0358f4e0ce7d012672048a298615dcbed86d8
Selorm Ahorlu <selormonray14@gmail.com>
2025-01-24 00:35:02 +0000

    progress on array list

--- arrayListAndList/src/MoreLists.java
+++ arrayListAndList/src/MoreLists.java
@@ -8,10 +8,15 @@ public class MoreLists {
 
         List<String> list = List.of(items);
         System.out.println(list);
-        System.out.println(list.getClass().getName());
 
-        ArrayList<String> groceries = new ArrayList<>();
+        ArrayList<String> groceries = new ArrayList<>(list);
         groceries.add("yogurt");
+        System.out.println(groceries);
+
+        ArrayList<String> nextList = new ArrayList<>(List.of("pickles", "mustard", "cheese"));
+        System.out.println(nextList);
+
+        groceries.addAll(nextList);
         System.out.println(groceries);
 
     }
