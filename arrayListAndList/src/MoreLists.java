64c2485087508a608d171f3676f1c145b23dfe25
Selorm Ahorlu <selormonray14@gmail.com>
2025-01-24 00:57:47 +0000

    adding and removing from an array list

--- arrayListAndList/src/MoreLists.java
+++ arrayListAndList/src/MoreLists.java
@@ -10,13 +10,35 @@ public class MoreLists {
         System.out.println(list);
 
         ArrayList<String> groceries = new ArrayList<>(list);
-        groceries.add("yogurt");
+        groceries.add("yoghurt");
         System.out.println(groceries);
 
         ArrayList<String> nextList = new ArrayList<>(List.of("pickles", "mustard", "cheese"));
         System.out.println(nextList);
 
         groceries.addAll(nextList);
+        System.out.println(groceries);
+
+        System.out.println("Third item = " + groceries.get(2));
+
+        if (groceries.contains("mustard")) {
+            System.out.println("List contains mustard");
+        }
+
+        groceries.add("yoghurt");
+        System.out.println("first = " + groceries.indexOf("yoghurt"));
+        System.out.println("last = " + groceries.lastIndexOf("yoghurt"));
+
+        System.out.println(groceries);
+        groceries.remove(1);
+        System.out.println(groceries);
+        groceries.remove("yoghurt");
+        System.out.println(groceries);
+
+        groceries.removeAll(List.of("apples", "eggs"));
+        System.out.println(groceries);
+
+        groceries.retainAll(List.of("apples", "milk", "mustard", "cheese"));
         System.out.println(groceries);
 
     }
