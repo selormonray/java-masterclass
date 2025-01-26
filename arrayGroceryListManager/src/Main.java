b14ba06717402556969904cdd2d429fe94aae686
Selorm Ahorlu <selormonray14@gmail.com>
2025-01-26 00:29:33 +0000

    Print Sorted List After Each Operation

--- arrayGroceryListManager/src/Main.java
+++ arrayGroceryListManager/src/Main.java
@@ -32,5 +32,7 @@ public class GroceryListManager {
                 default:
                     System.out.println("Invalid choice. Please try again.");
             }
-
+            printSortedGroceryList();
+        }
+        scanner.close();
 }
