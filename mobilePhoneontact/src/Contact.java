c0b29b79a099b9939fbafd83718d184f4a2f9e5d
Selorm Ahorlu <selormonray14@gmail.com>
2025-02-25 22:50:20 +0000

    created the main class

---
+++ mobilePhoneontact/src/Contact.java
@@ -0,0 +1,22 @@
+class Contact {
+    private final String name;
+    private final String phoneNumber;
+
+    public Contact(String name, String phoneNumber) {
+        this.name = name;
+        this.phoneNumber = phoneNumber;
+    }
+
+    public static Contact createContact(String name, String phoneNumber) {
+        return new Contact(name, phoneNumber);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+}
+
