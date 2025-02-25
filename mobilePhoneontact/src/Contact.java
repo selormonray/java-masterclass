6992aedadedb1b6fb7e1119dcb17056bac8ae210
Selorm Ahorlu <selormonray14@gmail.com>
2025-02-25 22:52:01 +0000

    updated the contact class

--- mobilePhoneontact/src/Contact.java
+++ mobilePhoneontact/src/Contact.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+
 class Contact {
     private final String name;
     private final String phoneNumber;
@@ -20,3 +22,67 @@ class Contact {
     }
 }
 
+class MobilePhone {
+    private final String myNumber;
+    private final ArrayList<Contact> myContacts;
+
+    public MobilePhone(String myNumber) {
+        this.myNumber = myNumber;
+        this.myContacts = new ArrayList<>();
+    }
+
+    public boolean addNewContact(Contact contact) {
+        if (findContact(contact.getName()) >= 0) {
+            return false;
+        }
+        myContacts.add(contact);
+        return true;
+    }
+
+    public boolean updateContact(Contact oldContact, Contact newContact) {
+        int position = findContact(oldContact);
+        if (position < 0) {
+            return false;
+        }
+        myContacts.set(position, newContact);
+        return true;
+    }
+
+    public boolean removeContact(Contact contact) {
+        int position = findContact(contact);
+        if (position < 0) {
+            return false;
+        }
+        myContacts.remove(position);
+        return true;
+    }
+
+    private int findContact(Contact contact) {
+        return myContacts.indexOf(contact);
+    }
+
+    private int findContact(String name) {
+        for (int i = 0; i < myContacts.size(); i++) {
+            if (myContacts.get(i).getName().equals(name)) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    public Contact queryContact(String name) {
+        int position = findContact(name);
+        if (position >= 0) {
+            return myContacts.get(position);
+        }
+        return null;
+    }
+
+    public void printContacts() {
+        System.out.println("Contact List:");
+        for (int i = 0; i < myContacts.size(); i++) {
+            Contact contact = myContacts.get(i);
+            System.out.println((i + 1) + ". " + contact.getName() + " -> " + contact.getPhoneNumber());
+        }
+    }
+}
